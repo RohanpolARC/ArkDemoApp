85979026b4f8a57a12662ecb6aa6a94ba7adf774
nmbharambe <nmbharambe@ivp.in>
2022-06-13 12:06:44 +0100

    Squashed commit of the following:
    commit 15262fc8eeca8e894e6a41c84d6dde7e8dae3e92
    Author: nmbharambe <nmbharambe@ivp.in>
    Date:   Mon Jun 13 12:04:58 2022 +0100
    
        AC-162. Added prod config

--- ArkWebApp/src/environments/environment.prod.ts
+++ ArkWebApp/src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   scopeUri: ['api://8ed8622e-3d1c-4f6b-842c-b22a74fc4209/ark-api'],  
   tenantId: '1aca2fbe-6078-4dfb-8c1f-9d98d6245214',  
   uiClienId: '6d8edbdf-e35f-4150-8e20-dbe7cf6f8c73',  
-  redirectUrl: 'https://arkweb.azurewebsites.net' 
+  redirectUrl: 'https://arkweb.azurewebsites.net',
+  irrCalcFunUrl: 'https://irrcalcfun.azurewebsites.net' 
 };
