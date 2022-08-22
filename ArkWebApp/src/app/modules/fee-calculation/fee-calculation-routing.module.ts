ee41b4c40d552194faf59ae02ac4f76f85737646
nmbharambe <nmbharambe@ivp.in>
2022-08-22 11:51:42 +0100

    AC-179. Fee calculation role guard applied.
    Added Fees tab group. Updated Arkfunction URL in environment.prod.

--- ArkWebApp/src/app/modules/fee-calculation/fee-calculation-routing.module.ts
+++ ArkWebApp/src/app/modules/fee-calculation/fee-calculation-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
+import { RoleGuard } from 'src/app/role.guard';
 import { FeeCalculationComponent } from './fee-calculation.component';
 
-const routes: Routes = [{ path: '', component: FeeCalculationComponent, canActivate: [MsalGuard], data: { tab: 'Fee Calculation' } }];
+const routes: Routes = [{ path: '', 
+component: FeeCalculationComponent, canActivate: [MsalGuard, RoleGuard], 
+data: { tab: 'Fee Calculation' } }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
