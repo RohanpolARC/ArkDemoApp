6cbc465bae00f9f08e5769e667527d33dc1eebf0
nmbharambe <nmbharambe@ivp.in>
2022-10-18 04:30:06 +0100

    Update fee-presets.component.ts

--- ArkWebApp/src/app/modules/fee-presets/fee-presets.component.ts
+++ ArkWebApp/src/app/modules/fee-presets/fee-presets.component.ts
@@ -182,7 +182,7 @@ export class FeePresetsComponent implements OnInit {
           DashboardTitle: ' '
         },
         Layout: {
-          Revision: 11,
+          Revision: 7,
           CurrentLayout: 'Default Layout',
           Layouts: [{
             Name: 'Default Layout',
@@ -191,7 +191,7 @@ export class FeePresetsComponent implements OnInit {
               ActionEdit: 'right' 
             },
             ColumnWidthMap: {
-              ActionEdit: 2
+              ActionEdit: 18
             }
           }]
         }
